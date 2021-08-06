594481b4ef6e8f5d0fccd6c3a0998fbc9d1590cb
artur haurylkevich <daniel1kdago@gmail.com>
2021-08-06 20:43:16 +0300

    cart component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,22 +1,36 @@
-import CategoryTitle from 'components/CategoryTitle'
 import React from 'react'
+
+import CategoryTitle from 'components/CategoryTitle'
+import Button from 'components/Button'
+
+import styled from 'styled-components'
+
+const CartItem = styled.li`
+  color: #000;
+  display: 'flex';
+`
 
 function Cart(props) {
   const ItemsInBag = () => {
     return props.items.map(item => (
-      <li key={item.id}>
-        {item.id} {item.title}{' '}
-        <button onClick={() => props.removeSelectionById(item.id, 'selected')}>
-          remove
-        </button>
-      </li>
+      <CartItem key={item.id}>
+        <img src={item.path} alt="sneakers" style={{ maxWidth: '200px' }} />
+        <div>
+          <p>{item.title}</p>
+          <Button
+            onClick={() => props.removeSelectionById(item.id, 'selected')}
+          >
+            remove
+          </Button>
+        </div>
+      </CartItem>
     ))
   }
 
   return (
     <div>
       <CategoryTitle>Cart items</CategoryTitle>
-      <ul>
+      <ul style={{ listStyle: 'none', display: 'flex', gap: '2em' }}>
         <ItemsInBag />
       </ul>
     </div>
