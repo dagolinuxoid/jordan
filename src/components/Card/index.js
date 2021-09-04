3a2b70fb1d4f94a846a9bf4a34dc0557476b857a
artur haurylkevich <daniel1kdago@gmail.com>
2021-09-04 11:03:55 +0300

    clean up

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,47 +2,45 @@ import Button from 'components/Button'
 
 import s from 'components/Card/Card.module.scss'
 
-const prefix = process.env.REACT_APP_IMG_PATH
-
 const Card = ({ title, price, id, selected, liked, path, updateChoice }) => {
-  return (
-    <li className={s.card}>
-      <img src={prefix + path} alt="Sneakers" className={s.pic} />
-      <div className={s.overlay}>
-        <div className={s.info}>
-          <div>{title}</div>
-          <div>${price}</div>
-        </div>
-        <div className={s.actions}>
-          <Button onClick={() => updateChoice(id, 'selected')}>
-            {selected ? 'Remove from cart' : 'Add to cart'}
-          </Button>
-          <div
-            onClick={() => updateChoice(id, 'liked')}
-            style={{
-              cursor: 'pointer',
-              border: '1px solid #ddd',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: '50%',
-              padding: 5,
-            }}
-          >
-            {liked ? (
-              <span className="material-icons" style={{ color: 'orange' }}>
-                favorite
-              </span>
-            ) : (
-              <span className="material-icons" style={{ color: '#666' }}>
-                favorite_border
-              </span>
-            )}
-          </div>
-        </div>
-      </div>
-    </li>
-  )
+	return (
+		<li className={s.card}>
+			<img src={path} alt="Sneakers" className={s.pic} />
+			<div className={s.overlay}>
+				<div className={s.info}>
+					<div>{title}</div>
+					<div>${price}</div>
+				</div>
+				<div className={s.actions}>
+					<Button onClick={() => updateChoice(id, 'selected')}>
+						{selected ? 'Remove from cart' : 'Add to cart'}
+					</Button>
+					<div
+						onClick={() => updateChoice(id, 'liked')}
+						style={{
+							cursor: 'pointer',
+							border: '1px solid #ddd',
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							borderRadius: '50%',
+							padding: 5,
+						}}
+					>
+						{liked ? (
+							<span className="material-icons" style={{ color: 'orange' }}>
+								favorite
+							</span>
+						) : (
+							<span className="material-icons" style={{ color: '#666' }}>
+								favorite_border
+							</span>
+						)}
+					</div>
+				</div>
+			</div>
+		</li>
+	)
 }
 
 export default Card
