28e46947d9e98d5b8448577738fd36a4afb09d57
artur haurylkevich <daniel1kdago@gmail.com>
2021-09-24 11:59:56 +0300

    update size

--- src/pages/Bookmarks.js
+++ src/pages/Bookmarks.js
@@ -4,35 +4,36 @@ import CategoryTitle from 'components/CategoryTitle'
 import Card from 'components/Card'
 
 const Bookmarks = props => {
-  const likedItems = props.allItems.filter(item => item.liked)
-  return (
-    <React.Fragment>
-      <CategoryTitle>My bookmarked sneakers</CategoryTitle>
-      <ul
-        style={{
-          display: 'flex',
-          gap: '2em',
-          marginBottom: '3em',
-          padding: '2em',
-        }}
-      >
-        {likedItems.map(fav => {
-          return (
-            <Card
-              key={fav.id}
-              title={fav.title}
-              id={fav.id}
-              selected={fav.selected}
-              liked={fav.liked}
-              path={fav.path}
-              price={fav.price}
-              updateChoice={props.updateChoice}
-            />
-          )
-        })}
-      </ul>
-    </React.Fragment>
-  )
+	const likedItems = props.allItems.filter(item => item.liked)
+	return (
+		<React.Fragment>
+			<CategoryTitle>My bookmarked sneakers</CategoryTitle>
+			<ul
+				style={{
+					display: 'flex',
+					gap: '2em',
+					marginBottom: '3em',
+					padding: '2em',
+					flexWrap: 'wrap',
+				}}
+			>
+				{likedItems.map(fav => {
+					return (
+						<Card
+							key={fav.id}
+							title={fav.title}
+							id={fav.id}
+							selected={fav.selected}
+							liked={fav.liked}
+							path={fav.path}
+							price={fav.price}
+							updateChoice={props.updateChoice}
+						/>
+					)
+				})}
+			</ul>
+		</React.Fragment>
+	)
 }
 
 export default Bookmarks
